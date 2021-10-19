4daf4462a8529bb498e5a4a413c2348f40ecee41
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2021-10-19 10:29:09 +0200

    add prefix "API"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,32 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersModule } from "./users/users.module";
-import { ProductsModule } from "./products/products.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule.forRoot()],
       useFactory: (configService: ConfigService) => ({
-        type: "mysql",
-        host: configService.get("MYSQL_HOST"),
-        port: +configService.get<number>("MYSQL_PORT"),
-        username: configService.get("MYSQL_USERNAME"),
-        password: configService.get("MYSQL_PASSWORD"),
-        database: configService.get("MYSQL_DB"),
-        entities: [__dirname + "/**/*.entity{.ts,.js}"],
-        synchronize: configService.get("NODE_ENV") === "development",
-        logging: configService.get("NODE_ENV") === "development"
+        type: 'mysql',
+        host: configService.get('MYSQL_HOST'),
+        port: +configService.get<number>('MYSQL_PORT'),
+        username: configService.get('MYSQL_USERNAME'),
+        password: configService.get('MYSQL_PASSWORD'),
+        database: configService.get('MYSQL_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') === 'development',
+        logging: configService.get('NODE_ENV') === 'development',
       }),
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     UsersModule,
-    ProductsModule
+    ProductsModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {}
