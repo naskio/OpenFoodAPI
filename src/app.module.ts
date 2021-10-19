5cdfb0d6b409a6916ed0c902dafd683301fae0b5
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2021-10-19 10:28:09 +0200

    add database (MYSQL) config + env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
+import { ProductsModule } from "./products/products.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      useFactory: (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get("MYSQL_HOST"),
+        port: +configService.get<number>("MYSQL_PORT"),
+        username: configService.get("MYSQL_USERNAME"),
+        password: configService.get("MYSQL_PASSWORD"),
+        database: configService.get("MYSQL_DB"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: configService.get("NODE_ENV") === "development",
+        logging: configService.get("NODE_ENV") === "development"
+      }),
+      inject: [ConfigService]
+    }),
+    UsersModule,
+    ProductsModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
